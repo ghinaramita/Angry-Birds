5fb60c2ce220df485bcc983037ffbd8b7ecd053b
ghinaramita <ghinaramita@gmail.com>
2021-09-22 19:39:12 +0700

    Commit 1

---
+++ Assets/Scripts/BombBird.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombBird : Bird
+{
+    public float radiusBomb;
+    public float force;
+    public GameObject ExplodeEffect;
+
+    public LayerMask LayerToHit;
+
+    public override void OnTap()
+    {
+        StartCoroutine(Explode(0f));
+        //Explode();
+    }
+
+    public override void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.tag == "Obstacle")
+            StartCoroutine(Explode(0.5f));
+        //Explode();
+    }
+
+    private IEnumerator Explode(float second)
+    {
+        yield return new WaitForSeconds(second);
+        Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, radiusBomb, LayerToHit);
+
+        foreach (Collider2D obj in objects)
+        {
+            Vector2 direction = obj.transform.position - transform.position;
+
+            obj.GetComponent<Rigidbody2D>().AddForce(direction * force);
+        }
+
+        GameObject ExplodeEffectIns = Instantiate(ExplodeEffect, transform.position, Quaternion.identity);
+        Destroy(ExplodeEffectIns, 1f);
+        Destroy(gameObject);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radiusBomb);
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public SlingShooter SlingShooter;
+    public TrailController trailController;
+
+    public GameObject PanelGameOver;
+    public GameObject PanelWin;
+
+    public List<Bird> Birds;
+    public List<Enemy> Enemies;
+
+    private Bird _shotBird;
+    public BoxCollider2D TapCollider;
+
+    public GameObject birdContainer;
+    public GameObject enemyContainer;
+
+    private bool _isGameEnded = false;
+
+    private void Awake()
+    {
+        Birds.AddRange(birdContainer.GetComponentsInChildren<Bird>());
+        Enemies.AddRange(enemyContainer.GetComponentsInChildren<Enemy>());
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < Birds.Count; i++)
+        {
+            Birds[i].OnBirdDestroyed += ChangeBird;
+            Birds[i].OnBirdShot += AssignTrail;
+        }
+
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            Enemies[i].OnEnemyDestroyed += CheckGameEnd;
+        }
+
+        TapCollider.enabled = false;
+        SlingShooter.InitiateBird(Birds[0]);
+        _shotBird = Birds[0];
+    }
+
+
+    public void ChangeBird()
+    {
+        TapCollider.enabled = false;
+
+        if (_isGameEnded)
+        {
+            return;
+        }
+
+        Birds.RemoveAt(0);
+
+        if (Birds.Count > 0)
+        {
+            SlingShooter.InitiateBird(Birds[0]);
+            _shotBird = Birds[0];
+        }
+
+        if (Birds.Count <= 0 && Enemies.Count >= 0)
+        {
+            StartCoroutine(LoseC(2f));
+        }
+    }
+
+    public void CheckGameEnd(GameObject destroyedEnemy)
+    {
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            if (Enemies[i].gameObject == destroyedEnemy)
+            {
+                Enemies.RemoveAt(i);
+                break;
+            }
+        }
+
+        if (Enemies.Count == 0 && Birds.Count >= 0)
+        {
+            _isGameEnded = true;
+            StartCoroutine(WinC(2f));
+        }
+    }
+
+    private IEnumerator LoseC(float second)
+    {
+        yield return new WaitForSeconds(second);
+        PanelGameOver.SetActive(true);
+    }
+
+    private IEnumerator WinC(float second)
+    {
+        yield return new WaitForSeconds(second);
+        PanelGameOver.SetActive(false);
+        PanelWin.SetActive(true);
+    }
+
+
+    public void AssignTrail(Bird bird)
+    {
+        trailController.SetBird(bird);
+        StartCoroutine(trailController.SpawnTrail());
+        TapCollider.enabled = true;
+    }
+
+    void OnMouseUp()
+    {
+        if (_shotBird != null)
+        {
+            _shotBird.OnTap();
+        }
+    }
+}
